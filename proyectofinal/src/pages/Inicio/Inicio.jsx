cfc3c7c7f0566e3e34bc213ed0041c7c15ba62e0
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2026-07-06 19:49:30 -0300

    Modificaciones en inicio y footer

--- proyectofinal/src/pages/Inicio/Inicio.jsx
+++ proyectofinal/src/pages/Inicio/Inicio.jsx
@@ -4,11 +4,12 @@ export function Inicio(){
     return (
     <>
     <div className="quienessomos">
-          <h2>Quienes somos</h2>
+          
            <div className="infoemprendimiento">
-            <p>Cookies Infernales es un emprendimiento ubicado en el barrio de Flores, que se especializa en la preparación y venta de galletitas estilo Nueva York. </p>
-            <p>Especialmente chocolate chip cookies que no van desde el sabor clásico para este tipo de galletitas sino también hasta sabores diferentes y complejos como cheesecake de arándanos, bananita dolca, vauquita, etc.</p>
-            <p>Pero también ofrece otros productos que varian desde la época del año hasta las festividades donde se pueden ver promociones especiales de productos no vistos comúnmente en este negocio.</p>
+            <h2>Quienes somos</h2>
+            <p className='info'>Cookies Infernales es un emprendimiento ubicado en el barrio de Flores, que se especializa en la preparación y venta de galletitas estilo Nueva York. <br />
+                Especialmente chocolate chip cookies que no van desde el sabor clásico para este tipo de galletitas sino también hasta sabores diferentes y complejos como cheesecake de arándanos, bananita dolca, vauquita, etc. <br />
+                Pero también ofrece otros productos que varian desde la época del año hasta las festividades donde se pueden ver promociones especiales de productos no vistos comúnmente en este negocio.</p>
             </div>
     </div>
     </>    
