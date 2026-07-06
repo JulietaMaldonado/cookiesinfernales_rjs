0ae730a440146fcee7cdca00e7d10929b2b50a5b
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2026-07-06 19:30:33 -0300

    Contenido y estilos para footer

--- proyectofinal/src/components/Footer/Footer.jsx
+++ proyectofinal/src/components/Footer/Footer.jsx
@@ -5,14 +5,38 @@ export function Footer() {
   return (
     <>
     <footer>
-      <div className="filafooter">
-        <div className="infofooter">
-          
+           <div class="redes">
+            <h5>REDES SOCIALES</h5>
+           <div class="redesfooter"> 
+            <a href="https://www.instagram.com/cookiesinfernales/">
+                <i class="fa-brands fa-instagram fa-2x"></i>
+            </a>
+        </div>
+    </div>
+        <div class="contactofooter">
+            <h5>CONTACTO</h5>
+            <ul>
+                <li>Whatsapp: (+54) 11-3442-5418</li>
+                <li>Mail: cookiesinfernales@gmail.com</li>
+            </ul>
         </div>
         <div className="tarjetasfooter">
-
-        </div>
-      </div>
+          <h5>EMPLEADOS</h5>
+          <div className="empletarjetas">
+             <div className="empleados">
+            <h6>Helena Barton</h6>
+            <p>Fundadora y CEO de Cookies Infernales.</p>
+          </div>
+          <div className="empleados">
+            <h6>Camille Simon</h6>
+            <p>Diseñadora gráfica principal de Cookies Infernales.</p>
+          </div>
+          <div className="empleados">
+            <h6>Ashley Carson</h6>
+            <p>Community manager principal de Cookies Infernales.</p>
+          </div>
+          </div>
+       </div>
     </footer>
     </>
   )
